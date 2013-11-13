c129d11001d4d921a7a01f250491fa82cbb58a56
jasonslyvia <jasonslyvia@gmail.com>
2013-11-13 18:14:07 +0800

    开始修改wookmark脚本

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+    <script src="<?php echo URL;?>/js/jquery.min.js">
+    </script>
+
+    <script type="text/javascript" src="<?php echo URL;?>/js/common.js">
+    </script>
+
+    <script type="text/javascript" src="<?php echo URL;?>/js/grid.js">
+    </script>
+
+  </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<!--[if lt IE 7 ]>
+<html class="no-js ie6">
+<![endif]-->
+<!--[if IE 7 ]>
+<html class="no-js ie7">
+<![endif]-->
+<!--[if IE 8 ]>
+<html class="no-js ie8">
+<![endif]-->
+<!--[if IE 9 ]>
+<html class="no-js ie9">
+<![endif]-->
+<!--[if (gt IE 9)|!(IE)]>
+<!-->
+<html class="no-js">
+<!--
+<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>小摄郎</title>
+
+    <meta name="robots" content="index,follow" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+    <meta name="google" content="notranslate" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link rel="icon" type="image/png" href="/favicon.png" />
+    <link rel="stylesheet" href="<?php echo URL;?>/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="<?php echo URL;?>/css/home.css" type="text/css" media="all" />
+
+    <script src="<?php echo URL;?>/js/modernizr.js"></script>
+
+</head>
+<body>
+    <!--  导航栏   -->
+    <div id="kaori" class="nodrag">
+
+      <div class="search">
+        <input type="text" name="term" autocomplete="off" />
+      </div>
+
+      <ol>
+
+        <li class="popular active" data-type="popular">
+          <a href="http://www.wookmark.com/" title="Popular on Wookmark">热门</a>
+        </li>
+
+        <li class="recent" data-type="recent">
+          <a href="http://www.wookmark.com/recent" title="New on Wookmark">最新</a>
+        </li>
+
+        <li class="categories">
+          <a href="http://www.wookmark.com/categories" title="Categories">分类</a>
+        </li>
+
+
+        <li class="colors">
+          <a href="http://www.wookmark.com/colors">色彩</a>
+        </li>
+
+      </ol>
+
+      <a href="http://www.wookmark.com/login" class="login">登录</a>
+
+      <p class="bottom">
+        <a href="http://www.wookmark.com/about/help">需要帮助？</a>
+      </p>
+    </div>
