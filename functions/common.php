8f6dd16b3ff8a06a01df4ce0191a4a26f7b82de2
jasonslyvia <jasonslyvia@gmail.com>
2013-11-22 17:39:10 +0800

    增加verify_ajax函数，协助验证ajax请求有效性

--- functions/common.php
+++ functions/common.php
@@ -5,6 +5,83 @@
 
 require_once('settings.php');
 
+/*
+ *  验证ajax请求是否合理
+ *
+ *  @param {array_n} keys 必须存在的参数名
+ *  @param {string} optional type HTTP请求方法，不填写则使用$_REQUESTS
+ *  @param {bool} optional check_nonce 对于涉及数据更改的操作验证nonce
+ *  @param {string} optional nonce_name 生成nonce生成的phase
+ *  @param {bool} optional display 是否直接返回结果
+ *  @return {bool}
+ */
+function verify_ajax($keys, $type = '_REQUESTS',
+                     $check_nonce = false, $nonce_name = '',
+                     $display = true)
+{
+    try {
+        //获得请求内容的数组
+        if (stripos($type, 'post') !== false) {
+            $request = $_POST;
+        }
+        else if (stripos($type, 'get') !== false) {
+            $request = $_GET;
+        }
+        else{
+            $request = $_REQUESTS;
+        }
+
+        //检查必填参数个数是否足够
+        if (count($request) < count($keys)) {
+            throw new Exception("请求参数异常", 1);
+        }
+
+
+        //使用 wp_verify_nonce 函数验证 nonce 有效性
+        if ($check_nonce && $nonce_name != '') {
+            define('WP_USE_THEMES', false);
+            require_once(ABSPATH.'wp-load.php');
+
+            //当前登录用户id
+            $uid = get_current_user_id();
+
+            //验证用户id设置是否正确
+            if (isset($request['userId']) || isset($request['user_id'])) {
+                $user_id = $request['userId'] ? $request['userId'] : $request['user_id'];
+                if ($user_id != $uid) {
+                    throw new Exception("参数userId：{$user_id}与当前用户id：{$uid}不符", 1);
+                }
+            }
+            /*
+                nonce的格式为 操作名称_用户id
+                             如：upload_pic_128
+            */
+            $nonce = $request['nonce'];
+            $nonce_name = preg_replace('/(_\d{1,})?/', '', $nonce_name);
+            $nonce_name .= "_{$uid}";
+            if (!wp_verify_nonce($nonce, $nonce_name)) {
+                throw new Exception("权限验证失败", 1);
+            }
+        }
+
+        //验证是否拥有所有的必须参数
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $request)) {
+                throw new Exception("缺少必要参数{$key}", 1);
+            }
+        }
+
+        return true;
+
+    } catch (Exception $e) {
+        if ($display) {
+            send_result(true, $e->getMessage());
+        }
+        else{
+            return false;
+        }
+    }
+}
 
 /*
  *  返回ajax结果
