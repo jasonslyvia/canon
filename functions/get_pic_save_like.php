013aa7bdc1c04e33e297d4b080cf6e8e676dedd3
jasonslyvia <jasonslyvia@gmail.com>
2013-11-22 16:17:53 +0800

    引入todo文件，保存图片处理

---
+++ functions/get_pic_save_like.php
@@ -0,0 +1,119 @@
+<?php
+/*
+ *  生成保存和喜欢的相关信息
+ *
+ *  @param {int} image_id 图片id（即post_id）
+ *  @param {int} user_id 用户id（即post_id）
+ *  @return {array_a}
+ */
+function get_pic_save_like($image_id, $user_id){
+
+    require_once('common.php');
+    define('WP_USE_THEMES', false);
+    require_once(ABSPATH.'wp-load.php');
+
+    $like_arr = get_pic_op($image_id, $user_id, "like");
+    $save_arr = get_pic_op($image_id, $user_id, "save");
+
+    return array("like" => $like_arr,
+                 "save" => $save_arr);
+
+}
+
+
+/*
+ *  获得保存或喜欢的详细数据
+ *
+ *  @param {int} image_id 图像id
+ *  @param {int} user_id 用户id
+ *  @param {string} type 指明类型是喜欢还是保存 可选值：like save
+ *  @return {array_a}
+ */
+function get_pic_op($image_id, $user_id, $type){
+
+    if ($type !== "save" && $type !== "like") {
+        return null;
+    }
+    $type_cn = $type == "save" ? "保存" : "喜欢";
+    $table = $type == "save" ? "pic_save" : "pic_like";
+
+    global $wpdb;
+    //当前用户是否喜欢或保存该图片
+    $current_user_op = $wpdb->get_var(
+            $wpdb->prepare("
+                    SELECT count(*) FROM {$table}
+                    WHERE pic_id = %d AND user_id = %d
+                ", $image_id, $user_id)
+    );
+    $current_user_op = $current_user_op == 0 ? '' : ' active';
+
+    //获取操作的数据
+    $op_row = $wpdb->get_results(
+        $wpdb->prepare("
+            SELECT SQL_CALC_FOUND_ROWS user_id, time FROM {$table}
+            WHERE pic_id = %d
+            ORDER BY time DESC
+            LIMIT 0,10
+        ", $image_id)
+    );
+
+
+    //用于显示简略版信息
+    $op_sample_html = "<p class='{$type}s'>";
+    //用于显示完整版信息
+    //<div class="group clearfix">
+    $op_list_html = '';
+
+    //若存在用户数据则进行渲染
+    if ($op_row) {
+        //总操作数
+        $op_count = $wpdb->get_var('SELECT FOUND_ROWS()');
+
+        //保存操作该图片的所有用户信息
+        $i = 0;
+        foreach ($op_row as $row) {
+            //操作该图片的用户id
+            $uid = $row->user_id;
+            //获取用户姓名
+            $name = get_userdata($uid)->display_name;
+            //获取用户头像
+            $avatar = AVATAR . 'default_avatar_small.png';
+
+            if ($i < 2) {
+                $op_sample_html .=
+                "<a href='/profile/{$uid}'>".
+                    "{$name}".
+                 "</a> ";
+            }
+
+            $op_list_html .=
+            "<a href='/profile/{$uid}' data-time='{$row->time}'>".
+                "<img src='{$avatar}' width='30' height='30' />".
+            "</a>".
+            "<p><a href='/profile/{$uid}'>{$name}</a> {$type_cn}了这张图片</a></p>";
+
+            $i++;
+        }
+
+        if ($op_count > 2) {
+            $real_count = $op_count - 2;
+            $op_sample_html .= " 和<b>另外{$real_count}个人</b>也{$type_cn}这张图片</p>";
+        }
+
+    }
+    //若无操作记录
+    else{
+        $op_count = 0;
+        $op_sample_html .= "做第一个{$type_cn}的人吧！</p>";
+    }
+
+    //将查询结果封装进数组
+    $arr["count"] = $op_count;
+    $arr["class_name"] = $current_user_op;
+    $arr["sample_html"] = $op_sample_html;
+    $arr["list_html"] = $op_list_html;
+
+    return $arr;
+}
+
+ ?>
---
+++ functions/save_pic.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ *  保存/取消保存一张图片
+ *
+ *  @param {int} imageId 图像id（即post_id）
+ *  @param {int} userId 用户id
+ *  @return {json}
+ */
+header('Content-Type: application/json');
+
+require('common.php');
+
+if (isset($_POST['imageId']) && isset($_POST['userId'])) {
+
+    define('WP_USE_THEMES', false);
+    require_once(ABSPATH.'wp-load.php');
+
+    $image_id = $_POST['imageId'];
+    $user_id = $_POST['userId'];
+
+    if (get_post($image_id) == null) {
+        send_result(true, "图片id{$image_id}不存在");
+    }
+    else if (get_user_by('id', $user_id) == false) {
+        send_result(true, "用户id{$user_id}不存在");
+    }
+
+    $URL = URL;
+
+    $html = <<<html
+<div id="groupsOverlay" data-imageId="{$image_id}">
+    <div class="wrap">
+        <div class="arrow">
+            <span class="up"></span>
+            <span class="up-wrapper"></span>
+        </div>
+        <h3><img src="{$URL}/img/check-circle.png" width="19" height="19">保存成功</h3>
+        <div class="topOptions">
+            <div class="closeButton actionButton"><span>完成</span></div>
+        </div>
+    </div>
+    <div class="options">
+        <div class="unsaveButton">取消保存</div>
+    </div>
+</div>
+html;
+    //首先检查记录是否存在
+    global $wpdb;
+    $save_row = $wpdb->get_row(
+            $wpdb->prepare('SELECT save_id FROM pic_save
+                            WHERE pic_id = %d
+                            AND user_id = %d',
+                            $image_id, $user_id)
+    );
+
+    //若保存记录存在，删除
+    if ($save_row ) {
+        // $deleted = $wpdb->delete('pic_save',
+        //                          array("save_id" => $save_row->save_id));
+        // if ($deleted) {
+
+        //     $save_count = get_post_meta($image_id, 'save_count', true);
+        //     update_post_meta($image_id, 'save_count', --$save_count);
+
+            send_result(false, "图片已保存", array("html" => $html));
+        // }
+        // else{
+        //     send_result(false, "取消保存失败");
+        // }
+    }
+    //若保存记录不存在，插入新纪录
+    else{
+        $inserted = $wpdb->insert('pic_save',
+                                array("pic_id" => $image_id,
+                                      "user_id" => $user_id),
+                                array("%d", "%d"));
+        //插入成功
+        if ($inserted !== false) {
+
+            $save_count = get_post_meta($image_id, 'save_count', true);
+            update_post_meta($image_id, 'save_count', ++$save_count);
+
+            send_result(false, "保存成功", array("html" => $html));
+        }
+        else{
+            send_result(true, "保存失败");
+        }
+    }
+
+}
+else{
+    send_result(true, "未知错误");
+}
+
+?>
