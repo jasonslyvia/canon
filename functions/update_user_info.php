d7e2d9fb898df3bc9cf60014ef505c0ac9319e49
jasonslyvia <jasonslyvia@gmail.com>
2013-11-26 22:33:06 +0800

    完成用户信息更新及修改密码功能

---
+++ functions/update_user_info.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ *  更新用户个人信息
+ *
+ *  @param {int} userId 用户id
+ *  @param {string} name 用户昵称（即wordpress中的display_name）
+ *  @param {string} email 用户邮箱（即 user_email）
+ *  @param {string} password 用户旧密码
+ *  @param {string} newPassword 用户新密码
+ *  @param {string} optional target 需要更新的内容，info 或 password，默认为 info
+ *  @return {json}
+ */
+header('Content-Type: application/json');
+require('common.php');
+
+//更新密码
+if ($_POST['target'] == "password") {
+    $required = array("password", "newPassword", "userId");
+    if (verify_ajax($required, "post", true, "user_info_action")) {
+        //验证新密码是否符合规则
+        $password = $_POST['password'];
+        $new_password = $_POST['newPassword'];
+        if (strlen($new_password) < 6 || strlen($new_password) > 16) {
+            send_result(true, "密码有效长度为 6 - 16 位");
+        }
+
+        define('WP_USE_THEMES', false);
+        require_once(ABSPATH.'wp-load.php');
+
+        //找到对应的用户
+        $user_id = $_POST['userId'];
+        $user = get_user_by('id', $user_id);
+        if (!$user) {
+            send_result(true, "未找到用户id为{$user_id}的用户");
+        }
+        else if (!wp_check_password($password, $user->data->user_pass, $user_id)) {
+            send_result(true, "原密码错误");
+        }
+        else{
+            wp_set_password($new_password, $user_id);
+            send_result(false, "密码更新成功！");
+        }
+    }
+}
+//更新基本信息
+else{
+    $required = array("userId", "name", "email");
+    //首先验证ajax请求的有效性
+    if (verify_ajax($required, "post", true, "user_info_action")) {
+
+        define('WP_USE_THEMES', false);
+        require_once(ABSPATH.'wp-load.php');
+
+        //找到对应的用户
+        $user_id = $_POST['userId'];
+        $user = get_user_by('id', $user_id);
+        if (!$user) {
+            send_result(true, "未找到用户id为{$user_id}的用户");
+        }
+
+        $name = $_POST['name'];
+        //验证数据有效性
+        if (empty($name)) {
+            send_result(true, "请设置您的昵称");
+        }
+        else{
+            if (mb_strlen($name, "utf8") < 2 &&
+                strlen($name, "utf8") > 10) {
+                send_result(true, "昵称有效长度为 2 - 10 个字符");
+            }
+            else if (!preg_match('/^[a-zA-Z0-9\x{4e00}-\x{9fa5}]+$/u', $name)) {
+                send_result(true, "昵称可包含的有效字符为大小写字母、数字或汉字");
+            }
+        }
+
+        $email = $_POST['email'];
+        if (empty($email)) {
+            send_result(true, "请设置您的邮件地址");
+        }
+        else if(!preg_match('/^\w+@\w+\.\w{2,4}$/', $email)){
+            send_result(true, "请输入有效的邮件地址");
+        }
+
+        //数据验证通过后更新用户数据
+        wp_update_user(array("ID" => $user_id,
+                             "display_name" => $name,
+                             "user_email" => $email));
+        send_result(false, "信息更新成功！");
+    }
+}
+
+
+?>
--- js/settings.js
+++ js/settings.js
@@ -44,4 +44,118 @@ $(function(){
         $(".avatar-preview .small").attr("src", ABSPATH + "/uploads/avatar/"+
                                         small);
     }
+
+
+    $("input").bind("keyup", function(e){
+        if (e.keyCode == 13) {
+            $(this).closest("div").find("input[type='button']").trigger("click");
+        }
+    });
+    //绑定基本信息更新
+    $("#infoSubmitBtn").bind("click", function(){
+        var $btn = $(this);
+        $(".error").remove();
+
+        //验证昵称字段的有效性
+        var name = $("#displayName").val();
+        if (!name) {
+            $btn.after("<span class='error'>昵称不得为空</span>");
+            return false;
+        }
+        else if (name.length < 2 || name.length > 10) {
+            $btn.after("<span class='error'>昵称长度为 2 - 10 个字符</span>");
+            return false;
+        }
+        else if (!name.match(/^[a-zA-Z0-9\u4e00-\u9fa5]+$/)) {
+            $btn.after("<span class='error'>昵称可包含的有效字符为大小写字母、数字或汉字</span>");
+            return false;
+        }
+
+        //验证邮箱的有效性
+        var email = $("#userEmail").val();
+         if (!email) {
+            $btn.after("<span class='error'>邮箱地址不得为空</span>");
+            return false;
+        }
+        else if (!email.match(/^\w+@\w+\.\w{2,4}$/)) {
+            $btn.after("<span class='error'>请输入有效的邮箱地址</span>");
+            return false;
+        }
+
+        $.ajax({
+            url: ABSPATH + '/functions/update_user_info.php',
+            type: 'POST',
+            data: {
+                name: name,
+                email: email,
+                userId: pageConfig.userId,
+                nonce: nonce
+            },
+            dataType: 'json',
+            success: function(data){
+                if (data.error) {
+                    $btn.after("<span class='error'>"+data.message+"</span>");
+                }
+                else{
+                    $btn.after("<span class='success'>更新成功</span>");
+                }
+                setTimeout(function(){
+                    $(".error, .success").fadeOut(function(){
+                        location.reload();
+                    });
+                }, 1000);
+            }
+        });
+    });
+
+    //绑定密码更新
+    $("#changePasswordBtn").bind("click", function(){
+        $(".error").remove();
+
+        var $btn = $(this);
+        var password = $("#currentPassword").val();
+        var newPassword = $("#newPassword").val();
+        var renewPassword = $("#renewPassword").val();
+
+        //验证数据有效性
+        if (!password || !newPassword || !renewPassword) {
+            $btn.after("<span class='error'>密码不能为空</span>");
+            return false;
+        }
+        else if (newPassword != renewPassword) {
+            $btn.after("<span class='error'>两次输入密码不一致</span>");
+            return false;
+        }
+        else if (newPassword.length < 6 || newPassword.length > 16) {
+            $btn.after("<span class='error'>密码有效长度为 6 - 16 位</span>");
+            return false;
+        }
+
+        //发送请求
+        $.ajax({
+            url: ABSPATH + '/functions/update_user_info.php',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                password: password,
+                newPassword: newPassword,
+                nonce: nonce,
+                userId: pageConfig.userId,
+                target: "password"
+            },
+            success: function(data){
+                if (data.error) {
+                    $btn.after("<span class='error'>"+data.message+"</span>");
+                }
+                else{
+                    $btn.after("<span class='success'>更新成功</span>");
+                    setTimeout(function(){
+                        $(".error, .success").fadeOut(function(){
+                            location.reload();
+                        });
+                    }, 1000);
+                }
+            }
+        });
+    });
 });
