ea04c058ffdc27be033f647021d4f907c6ef310c
jasonslyvia <jasonslyvia@gmail.com>
2014-01-01 08:38:14 +0800

    取消redirect.js，直接在事件绑定时执行未注册用户跳转逻辑

--- footer.php
+++ footer.php
@@ -18,27 +18,27 @@
     <script type="text/javascript" src="<?php echo URL;?>/js/common.js"> </script>
     <script type="text/javascript" src="<?php echo URL;?>/js/grid.js"> </script>
     <?php if (is_upload()): ?>
-        <script type="text/javascript" src="<?php echo URL;?>/js/jquery.uploadify.js"></script>
-        <script type="text/javascript" src="<?php echo URL;?>/js/upload.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/jquery.uploadify.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/upload.js"></script>
     <?php endif ?>
     <?php if (is_settings()): ?>
-        <script type="text/javascript" src="<?php echo URL;?>/js/jquery.uploadify.js"></script>
-        <script type="text/javascript" src="<?php echo URL;?>/js/settings.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/jquery.uploadify.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/settings.js"></script>
     <?php endif ?>
 
     <?php if (is_profile()): ?>
-        <script type="text/javascript" src="<?php echo URL;?>/js/user.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/user.js"></script>
     <?php endif ?>
 
     <?php if (is_single()): ?>
-        <script type="text/javascript" src="<?php echo URL;?>/js/image.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/image.js"></script>
     <?php endif ?>
 
     <?php if (is_edit()): ?>
-        <script type="text/javascript" src="<?php echo URL;?>/js/edit.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/edit.js"></script>
     <?php endif ?>
     <?php if (is_admin_front()): ?>
-        <script type="text/javascript" src="<?php echo URL;?>/js/admin.js"></script>
+    <script type="text/javascript" src="<?php echo URL;?>/js/admin.js"></script>
     <?php endif ?>
   </body>
 
