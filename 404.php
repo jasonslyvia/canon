fb95babccf6f0d50a5ae0386049a83e6cadf18d3
jasonslyvia <jasonslyvia@gmail.com>
2014-01-15 01:02:53 +0800

    增加404页面

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div id="luka">
+  <div class="hamburger"></div>
+  <p><a href="/"><?php echo get_bloginfo(); ?></a></p>
+</div>
+
+<div id="page">
+  <div class="headerSpacer"></div>
+
+  <div id="siteIntro">
+      <h2>服务器离家出走了……没有找到你想要看的页面！T_T</h2>
+      <a href="/" class="actionButton blueButton">返回首页</a>
+  </div>
+  <span class="logo-404">404</span>
+</div>
+
+<?php get_footer();?>
