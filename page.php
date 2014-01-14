54e88741fa84a204e4cf473049d159b8782e0fab
jasonslyvia <jasonslyvia@gmail.com>
2014-01-15 01:03:09 +0800

    增加page页面

--- page.php
+++ page.php
@@ -7,7 +7,25 @@
 
 <div id="page">
   <div class="headerSpacer"></div>
-  <div id="maincontent" class="center">
+
+<?php $pages = get_pages(array("sort_column" => "menu_order"));
+    if (count($pages)) {
+        echo '<ul class="page-menu clearfix">';
+        foreach ($pages as $page) {
+?>
+<li>
+    <a href="/<?php echo $page->post_name; ?>"<?php if (is_page($page->ID)) echo ' class="active"'; ?>>
+        <?php echo $page->post_title; ?>
+    </a>
+</li>
+<?php
+        }
+        echo '</ul>';
+    }
+?>
+  </ul>
+
+  <div id="maincontent" class="center page-content" style="width:700px;">
     <?php the_content(); ?>
   </div>
 </div>
