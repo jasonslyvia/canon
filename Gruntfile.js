83604b4ada34523d587ef68e527d3f1f442f3551
jasonslyvia <jasonslyvia@gmail.com>
2014-01-22 17:55:09 +0800

    更改gruntfile设置

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,13 +87,13 @@ module.exports = function(grunt) {
       afterBuild: ['.tmp']
     },
 
-    linkRev: {
+    wpreplace: {
       dist: {
         src: ['header-src.php', 'footer-src.php'],
         options: {
           templatePath: '/wp-content/themes/canon/',
-          jsPath: '/wp-content/themes/canon/js-dist/',
-          cssPath: '/wp-content/themes/canon/css-dist/',
+          jsPath: 'js-dist',
+          cssPath: 'css-dist',
           concat: [{
             src: ['common.js', 'grid.js'],
             dest: ['main.js']
@@ -111,141 +111,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-filerev');
-
-  grunt.registerMultiTask('linkRev', 'link filereved assets to certain path', function(){
-    var options = this.options();
-    var jsPath = options.jsPath;
-    var cssPath = options.cssPath;
-    var templatePath = options.templatePath;
-
-    //简化filerev保存的文件名修改信息，去掉多余的路径信息，只保留新旧文件名
-    //形如 {oldFileName.css : newFileName.css}
-    var newFiles = {};
-    for(var i in grunt.filerev.summary){
-      var newKey = i.split('/').pop();
-      newKey = newKey.replace(/\.min/i, '');
-      var newValue = grunt.filerev.summary[i].split('/').pop();
-
-      newFiles[newKey] = newValue;
-    }
-
-    //针对每一个文件进行遍历，依次进行正则匹配
-    this.files.forEach(function(file){
-      //首先确保文件存在
-      file.src.filter(function(filePath){
-        if (!grunt.file.exists(filePath)) {
-          grunt.log.warn('source file ' + filePath + ' not exist');
-          return false;
-        }
-        else{
-          return true;
-        }
-      }).forEach(function(filePath){
-
-        //获得文件内容
-        var content = grunt.file.read(filePath);
-        var cssRegExp = /(<link.*?href=[\'\"])(.*?)([\'\"][^>]*>)/gi;
-        var jsRegExp = /(<script.*?src=[\'\"])(.*?)([\'\"][^>]*?>\s*<\/script>)/gi;
-        var concat = options.concat;
-        var updated = false;
-        var cssCount = 0,
-            jsCount = 0;
-
-        //首先匹配出当前文件中的所有css引用
-        var match = cssRegExp.exec(content);
-        while(match){
-          var css = match[2].split('/').pop();
-          //若匹配filerev过的文件名
-          if (newFiles.hasOwnProperty(css)) {
-            var nCss = newFiles[css];
-
-            //若是style.css，
-            //将其从css-dist文件夹移到根目录下以便wordpress识别该主题
-            if (css.match(/^style\.css$/i)) {
-              grunt.file.copy('css-dist/'+nCss, nCss, {encoding: 'utf8'});
-              content = content.replace(new RegExp(match[0], "i"), match[1] + templatePath + nCss + match[3]);
-              grunt.log.subhead('style.css found! this is a Wordpress theme in no doubt!');
-            }
-            else{
-              content = content.replace(new RegExp(match[0], "i"), match[1] + cssPath + nCss + match[3]);
-            }
-            updated = true;
-            cssCount++;
-            grunt.log.debug('css replace: ' + css + ' : ' + nCss);
-          }
-          match = cssRegExp.exec(content);
-        }
-
-        //其次匹配当前文件中的所有js引用
-        //WARN：若文件中多次出现同一js的引入，同时存在concat的js
-        //      则可能出现将多行src js替换为1个concat后的js后，
-        //      正则匹配时跳过了若干个字符，参考js regexp.lastIndex
-        while((match = jsRegExp.exec(content)) !== null){
-          var js = match[2].split('/').pop();
-
-          //若该js被引用
-          if (newFiles.hasOwnProperty(js)) {
-
-            //判断其是否被concat
-            var concated = concat.every(function(el, i){
-              //若被concat
-              if(el.src.join(' ').indexOf(js) !== -1){
-                //若已替换则直接删除本条引用
-                if (el.replaced) {
-                  content = content.replace(new RegExp(match[0], "i"), '');
-                  //改变lastIndex确保匹配到文件中的每一个字符
-                  jsRegExp.lastIndex -= match[0].length;
-                  grunt.log.debug('remove concat src file ' + js);
-                }
-                //否则将concat后对应的文件替换，同时添加已替换属性
-                else{
-                  //若指定的concat后的文件不存在
-                  if (!grunt.file.exists('js-dist/'+newFiles[el.dest])) {
-                    grunt.fail.warn('concat file '+el.dest+' for '+el.src.join(',')+' does not exist!');
-                  }
-
-                  var concatedJs = match[1] + jsPath + newFiles[el.dest] + match[3];
-                  content = content.replace(new RegExp(match[0], "i"), concatedJs);
-                  //改变lastIndex确保匹配到文件中的每一个字符
-                  jsRegExp.lastIndex -= (match[0].length - concatedJs.length);
-                  el.replaced = true;
-
-                  grunt.log.debug('replace concat file '+newFiles[el.dest]+' for '+js);
-
-                  jsCount++;
-                  updated = true;
-                }
-
-                //不考虑a,b被concat为x，同时a,d被concat为y的情况
-                //不再继续遍历concat
-                return false;
-              }
-              return true;
-            });
-
-            //concated为true说明concat.every全部return true，即
-            //该js并未被concat
-            //因此直接替换为filerev后的文件即可
-            if (concated) {
-              content = content.replace(new RegExp(match[0], "i"), match[1] + jsPath + newFiles[js] + match[3]);
-              updated = true;
-              jsCount++;
-              grunt.log.debug('js replace: ' + js + ' : ' + newFiles[js]);
-            }
-          }
-
-        }
-
-        //如果替换了新的引用，则再次写入文件
-        if (updated) {
-          grunt.log.ok(cssCount + ' css and ' + jsCount + ' js are replaced in '+filePath);
-          filePath = filePath.replace(/-src/i, '');
-          grunt.file.write(filePath, content);
-        }
-      });
-    });
-
-  });
+  grunt.loadNpmTasks('grunt-wp-replace');
 
   grunt.registerTask('dist', ['clean:beforeBuild',
                                  'concat',
@@ -253,7 +119,7 @@ module.exports = function(grunt) {
                                  'cssmin',
                                  'filerev',
                                  'clean:afterBuild',
-                                 'linkRev'
+                                 'wpreplace'
                                  ]);
 
   grunt.registerTask('build', ['clean',
